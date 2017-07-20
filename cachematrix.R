a729106775ede81c2d2d2a173dbca604c7ede314
raymond25 <raymond.muliadi@gmail.com>
2017-07-20 09:15:21 -0400

    First Commit
    First commit for assignment 2 week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions help improving the time or speed taken in calculating an inverse matrix
+## by leveraging the caching process
 
-## Write a short comment describing this function
+## This function initializes the cached matrix to be inversed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  prev_matrix <- NULL
+  set <- function(y) {
+    x <<- y
+    prev_matrix <<- NULL
+  }
+  get <- function() x
+  setprevmatrix <- function(x) prev_matrix <<- x
+  getprevmatrix <- function() prev_matrix
+  list (set = set, get = get, setprevmatrix = setprevmatrix, getprevmatrix = getprevmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function will inverse the cached matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  prevmatrix <- x$getprevmatrix()
+
+  if(!is.null(prevmatrix)){
+    if(identical(x$get(),solve(prevmatrix))){
+      return(prevmatrix)
+    }
+  }
+  x$setprevmatrix(x)
+  m <- solve(x$get())
+  return(m)
 }
